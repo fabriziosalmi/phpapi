6a95b3cdc257252990c6879b5f9eef0ec5057fcf
fab <fabrizio.salmi@gmail.com>
2020-03-06 23:46:16 +0100

    Update api.php

--- ncov2/api.php
+++ ncov2/api.php
@@ -1,6 +1,4 @@
 <?php
-
-// use: http(s)://PHP_HOST/covid19_stats.php?date=03-03-2020
 // data source: https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports
 error_reporting(E_ERROR);
 
