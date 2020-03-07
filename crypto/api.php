9305fc9f601003431398372ec2fd443f5d47423f
fab <fabrizio.salmi@gmail.com>
2020-03-07 12:45:44 +0100

    Create api.php

---
+++ crypto/api.php
@@ -0,0 +1,31 @@
+<?php
+$coin = trim(htmlspecialchars($_GET["coin"]));
+$api = "https://api.nomics.com/v1/currencies/ticker?key=24d12bc4f4b4d6d845e3c36e0422bf78&ids=".$coin;
+$data = file_get_contents($api);
+$decode = json_decode($data, true);
+$name = $decode[0]["name"];
+$currency = $decode[0]["currency"];
+$price = $decode[0]["price"];
+$pricedate = $decode[0]["price_date"];
+$marketcap = $decode[0]["market_cap"];
+$circulating_supply = $decode[0]["circulating_supply"];
+// $volume = $decode[0]["volume"];
+
+$json = array(
+	"name" => $name,
+	"currency" => $currency,
+	"price ($)" => $price,
+	"price date" => $pricedate,
+	"marketcap" => $marketcap,
+	"circulating supply" => $circulating_supply
+//	"volume" => $volume
+);
+
+// JSON OUTPUT
+header('data-api: https://phpapi.org/crypto/');
+header('data-format: json');
+header('data-source: https://p.nomics.com/cryptocurrency-bitcoin-api');
+header('Content-Type: application/json');
+echo json_encode($json);
+
+?>
