94bc20df317fe30ab4985a0adac2e03348dbb06e
fab <fabrizio.salmi@gmail.com>
2020-03-09 21:51:21 +0100

    Update api.php

--- currency/api.php
+++ currency/api.php
@@ -1,13 +1,30 @@
 <?php
 
-$api = "https://api.ratesapi.io/api/latest";
-$data = file_get_contents($api);
+if ($_GET["EUR"]) == "") {
+  $json = array(
+    "error" => "input error"
+  );
+  
+  // JSON OUTPUT
+  header('data-api: https://phpapi.org/ncov2/');
+  header('data-format: json');
+  header('Content-Type: application/json');
+  echo json_encode($json);
+  exit;
+}
+  
+if (isset($_GET["EUR"])) {
 
-// JSON OUTPUT
-header('data-api: https://phpapi.org/ncov2/');
-header('data-format: json');
-header('data-source: https://api.ratesapi.io');
-header('Content-Type: application/json');
-echo json_encode($data);
+  $api = "https://api.ratesapi.io/api/latest";
+  $data = json_decode(file_get_contents($api));
+
+  // JSON OUTPUT
+  header('data-api: https://phpapi.org/ncov2/');
+  header('data-format: json');
+  header('data-source: https://api.ratesapi.io');
+  header('Content-Type: application/json');
+  echo json_encode($data);
+
+}
 
 ?>
