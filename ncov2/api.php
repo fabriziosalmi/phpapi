03646c3f5135c9ff19f4c21d3ec8200bd307ec28
fab <fabrizio.salmi@gmail.com>
2020-03-09 17:24:21 +0100

    Update api.php

--- ncov2/api.php
+++ ncov2/api.php
@@ -1,12 +1,11 @@
 <?php
 // data source: https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports
 error_reporting(E_ERROR);
-$ratio=($dsum*100)/$sum;
 
 // process data from https://github.com/CSSEGISandData/COVID-19
 $url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/".$_GET["date"].".csv";
 $data = file_get_contents($url);
-$temp_file = uniqid(rand(), true) . '.csv';
+$temp_file = "api_request.csv";
 file_put_contents($temp_file, $data);
 $csv = array_map('str_getcsv', file($temp_file));
 foreach($csv as $row) {
@@ -19,6 +18,8 @@ foreach($csv as $row) {
 }
 
 // JSON API
+$ratio=($dsum*100)/$sum;
+
 $json = array(
     "date" => $_GET["date"],
     "confirmed" => $sum,
@@ -33,6 +34,4 @@ header('data-source: https://github.com/CSSEGISandData/COVID-19/tree/master/csse
 header('Content-Type: application/json');
 echo json_encode($json);
 
-// clean up temp file
-unlink($temp_file);
 ?>
