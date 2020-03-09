04ae9fc53e55a13913b7067c4b48e0d017468d84
fab <fabrizio.salmi@gmail.com>
2020-03-09 11:41:46 +0100

    Update api.php

--- ncov2/api.php
+++ ncov2/api.php
@@ -2,10 +2,32 @@
 // data source: https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports
 error_reporting(E_ERROR);
 
-$url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/".$_GET["date"].".csv";
+// date check
+$input_date = htmlspecialchars($_GET["date"]);
+$regex = "/^(((0[13578]|1[02])\-(0[1-9]|[12]\d|3[01])\-((19|[2-9]\d)\d{2}))|((0[13456789]|1[012])\-(0[1-9]|[12]\d|30)\-((19|[2-9]\d)\d{2}))|(02\-(0[1-9]|1\d|2[0-8])\-((19|[2-9]\d)\d{2}))|(02\-29\-((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$/";
+$json = "";
+
+if (preg_match($regex, $input_date, $matches)){
+    $output = 1;
+}
+else {
+    return false;
+    $output = 0;
+	$json = array("error" => "data is not valid");
+	// JSON OUTPUT
+	header('data-api: https://phpapi.org/ncov2/');
+	header('data-format: json');
+	header('Content-Type: application/json');
+	echo json_encode($json);
+	exit;
+}
+
+
+$url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/".$input_date.".csv";
 $data = file_get_contents($url);
-file_put_contents("last.csv", $data);
-$csv = array_map('str_getcsv', file('last.csv'));
+$temp_file = ("last_".$rand(11111,22222));
+file_put_contents($temp_file, $data);
+$csv = array_map('str_getcsv', file($temp_file));
 foreach($csv as $row) {
 	$cases = $row[3];
 	$dead = $row[4];
@@ -16,6 +38,9 @@ foreach($csv as $row) {
 }
 
 $ratio=($dsum*100)/$sum;
+
+// clean up temp file
+unlink($temp_file);
 
 // JSON API
 $json = array(
