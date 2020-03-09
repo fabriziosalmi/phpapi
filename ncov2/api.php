26010051303cbd8457f6a8b801a5c04f95449a54
fab <fabrizio.salmi@gmail.com>
2020-03-09 17:22:54 +0100

    Update api.php

--- ncov2/api.php
+++ ncov2/api.php
@@ -29,7 +29,6 @@ $json = array(
 // JSON OUTPUT
 header('data-api: https://phpapi.org/ncov2/');
 header('data-format: json');
-header('data-request: $GET["url"]');
 header('data-source: https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports');
 header('Content-Type: application/json');
 echo json_encode($json);
