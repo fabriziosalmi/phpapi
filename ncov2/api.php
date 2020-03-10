e9dd87f1d6368d4039a366e0c5b9903125e4df2b
fab <fabrizio.salmi@gmail.com>
2020-03-10 22:19:41 +0100

    Update api.php

--- ncov2/api.php
+++ ncov2/api.php
@@ -2,37 +2,36 @@
 // data source: https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports
 error_reporting(E_ERROR);
 $json = "";
+$date = $_GET["date"];
+$country = $_GET["country"];
 
 $url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/".$_GET["date"].".csv";
 $data = file_get_contents($url);
 $temp_file = uniqid(rand(), true) . '.csv';
 file_put_contents($temp_file, $data);
 $csv = array_map('str_getcsv', file($temp_file));
-foreach($csv as $row) {
-	$cases = $row[3];
-	$dead = $row[4];
-	$sum+= $cases;
-	$dsum+= $dead;
-   foreach($row as $k) {
-   	}
+
+
+foreach ($csv as $row) {
+
+	if ($row[1] == $_GET["country"]) {
+		
+		$ratio = ($row[4]*100)/$row[3];
+		
+	$json = array(
+		"country" => $row[1],
+		"confirmed" => $row[3],
+		"deaths" => $row[4],
+		"ratio" => round($ratio, 2)
+	);
+
+		// JSON OUTPUT
+		header('data-api: https://phpapi.org/ncov2/');
+		header('data-format: json');
+		header('data-source: https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports');
+		header('Content-Type: application/json');
+		echo json_encode($json);	
+	} 
 }
-
-$ratio=($dsum*100)/$sum;
-
-// JSON API
-$json = array(
-    "date" => $_GET["date"],
-    "confirmed" => $sum,
-    "deaths" => $dsum,
-    "ratio" => round($ratio, 3)
-    );
-
-// JSON OUTPUT
-header('data-api: https://phpapi.org/ncov2/');
-header('data-format: json');
-header('data-source: https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports');
-header('Content-Type: application/json');
-echo json_encode($json);
-
 
 ?>
